5d3d61a90128fc98e5303bd1991bea01114c9cbd
harish001 <127609257+Hdgy7@users.noreply.github.com>
2025-05-12 00:44:22 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create a base axios instance with your API base URL
 const instance = axios.create({
-  baseURL: 'https://attendance-app-xtnq.onrender.com/', // Your backend URL
+  baseURL: 'https://attendance-app-xtnq.onrender.com/api/', // Your backend URL
   withCredentials: true, // Make sure credentials (cookies) are sent along with requests
 });
 
