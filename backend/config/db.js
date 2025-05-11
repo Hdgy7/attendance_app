096ac7fd99b908c02cf875cbe3116390f47fe644
harish001 <127609257+Hdgy7@users.noreply.github.com>
2025-05-12 00:12:30 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'rdbms',
-  database: 'attendance_management'
+  host: 'sql12.freesqldatabase.com',
+  user: 'sql12778224',
+  password: '8Y47NaPdiD',
+  database: 'sql12778224'
 });
 
 connection.connect(err => {
